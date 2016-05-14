098e65555d980f036d0e5640fc5e4a146523a3fd
一言不合就卖萌 <iamwpz@qq.com>
2016-05-14 18:23:21 +0800

    设置页面诞生啦哈哈哈哈哈哈

---
+++ SunMoFruit/SunMoFruit/Classes/PersonCenter/Controller/SMSettingVC.h
@@ -0,0 +1,13 @@
+//
+//  SMSettingVC.h
+//  SunMoFruit
+//
+//  Created by 沛之 on 16/5/14.
+//  Copyright © 2016年 hunter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMSettingVC : UIViewController
+
+@end
